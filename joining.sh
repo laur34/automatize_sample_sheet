a22bba1a661315fd6b67d16a4ca60c0247722982
laur34 <lv70xo@gmail.com>
2019-11-19 14:42:21 +0100

    changed joining.sh endings to merged.fq

--- joining.sh
+++ joining.sh
@@ -5,3 +5,4 @@ sort -k1,1 -t $'\t' input2.tsv > input2sorted.tsv
 
 join -j1 input2sorted.tsv input1sorted.tsv -t $'\t' > joined.tsv
 cut -f2,4,5 joined.tsv > samplesheet.txt
+sed -i 's/L001_R1_001.fastq/L001_merged.fq/' samplesheet.txt
