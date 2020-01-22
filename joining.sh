e5859fc9a3d501331e190efd78356845de193e9a
laur34 <lv70xo@gmail.com>
2020-01-22 13:14:23 +0100

    deleted no-longer-needed scripts

--- joining.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-sort -k1,1 -t $'\t' input1.tsv > input1sorted.tsv
-sort -k1,1 -t $'\t' input2.tsv > input2sorted.tsv
-
-join -j1 input2sorted.tsv input1sorted.tsv -t $'\t' > joined.tsv
-cut -f2,4,5 joined.tsv > samplesheet.txt
-sed -i 's/L001_R1_001.fastq/L001_merged.fq/' samplesheet.txt
